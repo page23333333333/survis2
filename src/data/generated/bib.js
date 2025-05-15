a5c1de5505a9407213830356e855b005f4af7e65
page23333333333 <alypx4@nottingham.ac.uk>
2025-05-15 22:13:30 +0800

    Add files via upload

---
+++ src/data/generated/bib.js
@@ -0,0 +1,89 @@
+﻿define({ entries : {
+    "agostinelli2023musiclm": {
+        "author": "Agostinelli, Andrea and Carlini, Nicholas and Clark, Jack and others",
+        "journal": "arXiv preprint arXiv:2301.11325",
+        "title": "MusicLM: Generating Music From Text",
+        "type": "article",
+        "year": "2023"
+    },
+    "brown2020gpt3": {
+        "author": "Brown, Tom B. and Mann, Benjamin and Ryder, Nick and others",
+        "doi": "10.48550/arXiv.2005.14165",
+        "journal": "Advances in Neural Information Processing Systems",
+        "pages": "1877--1901",
+        "title": "Language Models are Few-Shot Learners",
+        "type": "article",
+        "volume": "33",
+        "year": "2020"
+    },
+    "karras2019stylegan": {
+        "author": "Karras, Tero and Laine, Samuli and Aila, Timo",
+        "booktitle": "Proceedings of the IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR)",
+        "doi": "10.1109/CVPR.2019.00453",
+        "pages": "4401--4410",
+        "title": "A Style-Based Generator Architecture for Generative Adversarial Networks",
+        "type": "inproceedings",
+        "year": "2019"
+    },
+    "li2023surveyGPT": {
+        "author": "Li, Jiaqing and Liu, Hang and Zhang, Chen and Zhou, Yifan",
+        "doi": "10.1145/3591947",
+        "journal": "ACM Computing Surveys",
+        "number": "12",
+        "pages": "1--38",
+        "title": "Generative Pre-trained Transformers: A Survey",
+        "type": "article",
+        "volume": "55",
+        "year": "2023"
+    },
+    "ramesh2021dalle": {
+        "author": "Ramesh, Aditya and Pavlov, Mikhail and Goh, Gabriel and Gray, Scott and others",
+        "journal": "arXiv preprint arXiv:2102.12092",
+        "title": "Zero-Shot Text-to-Image Generation",
+        "type": "article",
+        "year": "2021"
+    },
+    "saharia2022imagen": {
+        "author": "Saharia, Chitwan and Chan, William and Saxena, Saurabh and Li, Lala and Salimans, Tim and others",
+        "booktitle": "International Conference on Machine Learning (ICML)",
+        "pages": "18378--18392",
+        "title": "Photorealistic Text-to-Image Diffusion Models",
+        "type": "inproceedings",
+        "year": "2022"
+    },
+    "theis2016evaluation": {
+        "author": "Theis, Lucas and van den Oord, Aaron and Bethge, Matthias",
+        "journal": "International Conference on Learning Representations (ICLR)",
+        "title": "A Note on the Evaluation of Generative Models",
+        "type": "article",
+        "year": "2016"
+    },
+    "wang2021surveyGAN": {
+        "author": "Wang, Wei and Liu, Zhen and Zhu, Yi and Tang, Jian",
+        "doi": "10.1109/TNNLS.2020.3015466",
+        "journal": "IEEE Transactions on Neural Networks and Learning Systems",
+        "number": "8",
+        "pages": "3213--3233",
+        "title": "A Survey on Generative Adversarial Networks: Variants, Applications, and Training",
+        "type": "article",
+        "volume": "32",
+        "year": "2021"
+    },
+    "yang2022diffusionSurvey": {
+        "author": "Yang, Lin and Zhang, Cheng and Ma, Hongyu and Wang, Jianmin",
+        "journal": "arXiv preprint arXiv:2209.00796",
+        "title": "Diffusion Models: A Comprehensive Survey of Methods and Applications",
+        "type": "article",
+        "year": "2022"
+    },
+    "zhang2023multimodalSurvey": {
+        "author": "Zhang, Li and Xu, Ying and Gao, Fei and others",
+        "doi": "10.1109/ACCESS.2023.3257181",
+        "journal": "IEEE Access",
+        "pages": "42000--42020",
+        "title": "Emerging Trends in Generative AI for Multimodal Content Creation: A Survey",
+        "type": "article",
+        "volume": "11",
+        "year": "2023"
+    }
+}});
